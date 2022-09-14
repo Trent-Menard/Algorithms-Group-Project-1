9538e638ea2f6b6473d292d84ed3288b5763fee8
Trent Menard <25115762+Trent-Menard@users.noreply.github.com>
2022-09-14 14:12:06 -0500

    Add minor error handling for input functions

--- RSA.py
+++ RSA.py
@@ -6,13 +6,14 @@ Created on Tue Sep 13 11:37:11 2022
 """
 
 import math
+import sys
 import pandas as pd #Dr. Hu said he wasn't against us using pandas
 
-class PublicUser:
-    public_key = ""
-    private_key = ""
+# class PublicUser:
+#     public_key = ""
+#     private_key = ""
     
-# How to choose parameters for pow() to get p & q? Random Num Gen? What for min & max?
+# Use Fermat's Little Theorem for parameters of pow() to gen p & q
 # base, power, %
 p_val = pow(30192, 43791, 65301)
 q_val = pow(30192, 43791, 65301)
@@ -20,43 +21,45 @@ public_key = 0;
 phi = 0;
 e = 0;
 
-# Random Num Gen?
-    if math.gcd(x, phi) == 1:
-        x = e;
-        break
-
-#test to store messages inside a dataframe
-#only 
+    # if math.gcd(x, phi) == 1:
+    #     x = e;
+    #     break
 
 def encrypted_message():
     #create a dictionary to store more than one message?
+    pass
+    
     
 def sign_message(sign):
     #I guess we just create a variable for the signature?
+    pass
     
 
 def public_user():
-    ans = 0
-    ans = int(input("\nAs a public user, what would you like to do?\n\t
-                    "1. Send an encrypted message\n\t
-                    "2. Authenticate a digital signature\n\t
-                    "3. Exit\n
-                    "Enter your choice: "))
+    ans = int(input("\nAs a public user, what would you like to do?\n" +
+                    "1. Send an encrypted message.\n" +
+                    "2. Authenticate a digital signature.\n" + 
+                    "3. Exit.\n" +
+                    "\n Enter your choice: "))
+    
     if ans == 1:
         message = input("\nEnter a message: ")
         #call funtion to encrypt a message (message)
     elif ans == 2:
         #call funtion to authenticate a digital signature
+        pass
+    elif ans == 3:
+        sys.exit("Goodbye.")
     else:
-        prompt()
+        print("Invalid choice.")
+        public_user()
     
 def owner():
-    ans = 0
-    ans = int(input("\nAs the owner of the keys, what would you like to do?\n\t
-                    "1. Decrypt a received message
-                    "2. Digitally sign a message
-                    "3. Exit\n
-                    "Enter your choice: "))
+    ans = int(input("\nAs the owner of the keys, what would you like to do?\n" +
+                    "1. Decrypt a received message.\n" +
+                    "2. Digitally sign a message.\n" +
+                    "3. Exit.\n" +
+                    "\nEnter your choice: "))
     if ans == 1:
         #call function to list encrypted messages
         print("\nThe following messages are available:\n\t")
@@ -64,19 +67,28 @@ def owner():
         #call function to signs a message
         sig = input("\nEnter a message: ")
         sign_message(sig)
+    elif ans == 3:
+        sys.exit("Goodbye.")
+    else:
+        print("Invalid choice.")
+        owner()
         
 def prompt():
-    ans = 0
-    ans = int(input("Please select your user type\n\t"
-          "1. A public user\n\t"
-          "2. The owner of the keys\n\t"
-          "3. Exit program\n"
-          "Enter your choice: "))
+    ans = int(input("Please select your user type\n" +
+          "1. A public user.\n" +
+          "2. The owner of the keys.\n" +
+          "3. Exit program\n" +
+          "\nEnter your choice: "))
+    
     if ans == 1:
         public_user()
     elif ans == 2:
         owner()
+    elif ans == 3:
+        sys.exit("Goodbye.")
     else:
-        #return
-        
-    
+        print("Invalid choice.")
+        prompt()
+
+
+prompt()
