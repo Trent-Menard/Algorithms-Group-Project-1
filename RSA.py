111f44871aed7026c5822fd5fdcc8ab092af3d38
Trent Menard <25115762+Trent-Menard@users.noreply.github.com>
2022-09-14 13:34:54 -0500

    Merge

--- RSA.py
+++ RSA.py
@@ -6,13 +6,12 @@ Created on Tue Sep 13 11:37:11 2022
 """
 
 import math
-import pandas as pd #Dr. Hu said he wasn't against us using pandas
 
 class PublicUser:
     public_key = ""
     private_key = ""
     
-# How to choose/generate parameters for pow() to get p & q?
+# How to choose parameters for pow() to get p & q? Random Num Gen? What for min & max?
 # base, power, %
 p_val = pow(30192, 43791, 65301)
 q_val = pow(30192, 43791, 65301)
@@ -20,7 +19,7 @@ public_key = 0;
 phi = 0;
 e = 0;
 
-for x in range(phi):
+# Random Num Gen?
     if math.gcd(x, phi) == 1:
         x = e;
         break
