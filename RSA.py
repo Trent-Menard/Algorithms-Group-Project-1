6c2778ea6ab158d492f0cac2d0b8696c5cd249fd
Trent Menard <trentmenard6@yahoo.com>
2022-09-20 00:19:50 -0500

    Update P & Q generation, implement interactive encryption, code cleanup

--- RSA.py
+++ RSA.py
@@ -7,59 +7,45 @@ Created on Tue Sep 13 11:37:11 2022
 
 import sys
 import random
+import math
 
 # class PublicUser:
 #     public_key = ""
 #     private_key = ""
     
-# Use Fermat's Little Theorem for parameters of pow() to gen p & q
-# Large prime numbers for testing; from https://bigprimes.org/
-MIN_PRIME_NUMBER = 1000000
-MAX_PRIME_NUMBER = 10000000
+# Fermat's Little Theorem for parameters of pow() (Fast Modular Exponentantiaion) to gen p & q
+MIN_PRIME_NUMBER = 1_000_000
+MAX_PRIME_NUMBER = 10_000_000
 
-# Generate random p & q val between 1 & 10 mil
+# Generate random p & q val between 1 & 10 million
 p_val = random.randint(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER)
-q_val = random.randint(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER) 
+q_val = random.randint(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER)
 all_candidates = set()
 prime_candidates = set()
 failed_prime_candidates = set()
 
+phi = (p_val - 1) * (q_val - 1)
+e = random.randint(1, phi + 1)
+n = p_val * q_val
+
+# Generate random e (public key) using Euclid's algorithm until its relatively prime to phi
+while not math.gcd(e, phi) == 1: 
+    e = random.randint(1, phi + 1)
+    
 def FermatPrimalityTest(n):
     # Arbitrairly test 20 values within range to verify primality
-    for a in range(1, 20):
+    for test in range(1, 20):
         test = random.randint(MIN_PRIME_NUMBER + 1, MAX_PRIME_NUMBER - 1)
         all_candidates.add(test)
         
-        if pow(test, n-1, n) == 1:
-            prime_candidates.add(n)
-        else:
+        if pow(test, n-1, n) != 1:
             failed_prime_candidates.add(n)
-            # print(p, " is NOT prime.")
-            # return False
-        
-    # print(p, " is prime!")
+            return False
+        else:
+            prime_candidates.add(n)
+            
+    # Pass Fermat test if all 20 tests pass
     return True
-    
-# If p_val is not prime, generate another p_val
-while not FermatPrimalityTest(p_val):
-    p_val = random.randint(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER)
-    
-# If q_val is not prime, generate another q_val
-while not FermatPrimalityTest(q_val):
-    q_val = random.randint(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER)
-    
-    
-print("Generated p_val = " + str(p_val) + " & q_val = " + str(q_val) + "\n")
-print("Success: " + str(prime_candidates) + "\n")
-print("Failed: " + str(failed_prime_candidates) + "\n")
-print("All Candidates: " + str(all_candidates) + "\n")
-print("Error Ratio: " + str(len(failed_prime_candidates) / len(all_candidates)) + "%")
-
-# print("D: ",d)
-# print("E: ",e)
-# print("N: ",n)
-# print("Phi: ",phi)
-# print(e)
 
 def encrypted_message():
     #create a dictionary to store more than one message?
@@ -79,7 +65,7 @@ def public_user():
     if ans == 1:
         message = input("\nEnter a message: ")
         #call funtion to encrypt a message (message)
-        # encrypt(message,e,n)
+        encrypt(message,e,n)
     elif ans == 2:
         #call funtion to authenticate a digital signature
         pass
@@ -127,10 +113,12 @@ def prompt():
         
 def encrypt(message,e,n):
     upper_msg = message.upper()
-    encrypted = [ord(x) for x in upper_msg] # generate numerical alphabet
-    print("\nASCII VALUES OF MESSAGE: ",encrypted, "\n") # remove this line when ready
-    encryptedM = [pow(m,e,n) for m in encrypted] # encrypt each letter
-    print("C = (M^e)modn --> ", encryptedM, "\n") # remove this line when ready
+    # Map char -> ASCII code
+    encrypted = [ord(x) for x in upper_msg]
+    print("\nASCII VALUES OF MESSAGE: ",encrypted, "\n")
+    # Encrypt each ASCII code using Fast Modular Exponentiation with Public Key
+    encryptedM = [pow(m,e,n) for m in encrypted]
+    print("C = (M^e)modn --> ", encryptedM, "\n")
     return encryptedM
     
 def decrypt(e_msg,d,n):
@@ -142,11 +130,25 @@ def decrypt(e_msg,d,n):
         finish += x
     return finish
     
-def e_gcd(a =1, b = 1):
+def e_gcd(a = 1, b = 1):
     if b == 0:
         return (1, 0, a)
     (x, y, d) = e_gcd(b, a%b)
     return y, x - a//b*y, d
 
-# encryptM = encrypt("Ths message is top secret af",e,n)
-# print(decrypt(encryptM,d,n))
+prompt()
+
+# If p_val is not prime, generate another p_val
+while not FermatPrimalityTest(p_val):
+    p_val = random.randint(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER)
+    
+# If q_val is not prime, generate another q_val
+while not FermatPrimalityTest(q_val):
+    q_val = random.randint(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER)
+    
+print("Generated p_val = " + str(p_val) + " & q_val = " + str(q_val) + "\n")
+print("Success: " + str(prime_candidates) + "\n")
+print("Failed: " + str(failed_prime_candidates) + "\n")
+print("All Candidates: " + str(all_candidates) + "\n")
+print("Prime/All Candidates Ratio: " + str(len(prime_candidates) / len(all_candidates)) + "%")
+print("Prime/Failed Candidates Ratio: " + str(len(prime_candidates) / len(failed_prime_candidates)) + "%")
